02e1db3e36dab184657cae57a4075bc08a945f3e
AkshitRathi07 <aksethrathi@gmail.com>
2026-01-28 18:32:40 +0530

    final textation code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import React from 'react'                    //rcc
 import NavBar from './components/NavBar'
 import News from './components/News'
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, } from "react-router-dom"
 import LoadingBar from 'react-top-loading-bar'
 
 
